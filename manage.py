48d76d4fc331e2d32643cb68d298f87e33d4ee30
Marty Lei <32186394+Martylei@users.noreply.github.com>
2020-06-12 10:15:05 +0800

    Add files via upload

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite_for_gallary.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'new_mysite_for_gallery.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
---
+++ new_site_for_gallery/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NewSiteForGalleryConfig(AppConfig):
+    name = 'new_site_for_gallery'
